3f996ba163e3aa25b2d45273078dcc48edaffb0c
Sanskar Omar <heysanskar0.0@gmail.com>
2023-11-08 19:24:53 +0530

    Implemented pptx to pdf conversion functionality

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import os
 from pptx import Presentation
+import comtypes.client
+
+
+def PPT_to_PDF(input_pptx, output_pdf, formatType=32):
+    powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
+    powerpoint.Visible = 1
+
+    if output_pdf[-3:] != "pdf":
+        output_pdf = output_pdf + ".pdf"
+    deck = powerpoint.Presentations.Open(input_pptx)
+    deck.SaveAs(output_pdf, formatType)  # formatType = 32 for ppt to pdf
+    deck.Close()
+    powerpoint.Quit()
+
 
 # Load the existing PowerPoint presentation
-input_pptx = 'existing_presentation.pptx'
-output_pdf = 'output_presentation.pdf'
+input_pptx = "existing_presentation.pptx"
+output_pdf = "output_presentation.pdf"
+
+base_path = os.path.dirname(os.path.abspath(__file__))
 
 prs = Presentation(input_pptx)
 
 # Define the placeholder text to replace
-placeholder_name = 'Placeholder_Name'
+placeholder_name = "Placeholder_Name"
 
 # Get user input for the replacement name
 replacement_name = input("Enter the name to replace '{}': ".format(placeholder_name))
@@ -22,7 +39,10 @@ for slide in prs.slides:
                         run.text = run.text.replace(placeholder_name, replacement_name)
 
 # Save the modified PowerPoint presentation
-updated_pptx = 'updated_presentation.pptx'
+updated_pptx = "updated_presentation.pptx"
 prs.save(updated_pptx)
+
+# Convert the updated PowerPoint presentation to PDF
+PPT_to_PDF(os.path.join(base_path, updated_pptx), os.path.join(base_path, output_pdf))
 
 print(f"The presentation has been updated and saved as '{updated_pptx}'.")
